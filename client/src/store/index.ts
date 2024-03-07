52bd07c38f2b153706ae05064b06be39fd28a39e
Vitaliy Sereda <1239874560ret@gmail.com>
2024-03-07 16:35:51 +0200

    Added slice for auth

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,9 +1,13 @@
 import {configureStore} from '@reduxjs/toolkit'
 import {todoReducer} from './todoSlice'
+import auth from './authSlice'
+import { api } from './services/api';
 
 const store = configureStore({
     reducer: {
         todos: todoReducer,
+        [api.reducerPath]: api.reducer,
+        auth
     }
 })
 
